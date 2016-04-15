30b870db3ebd1a0d50bcd5e88d781cb91922e271
Kevin Dunn <kevindunn@kevin-dunn.local>
2016-04-15 15:47:32 +0200

    Initial Commit

---
+++ delftdemo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  delftdemo
+//
+//  Created by Kevin Dunn on 2016-04-15.
+//  Copyright © 2016 Kevin Dunn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
